dec85f03966db58a26f5a3da9d29c0887e3cdbd2
Alexandr23 <a.nikiforov@magdv.com>
2017-05-10 08:49:22 +0700

    try to setup ssr

--- src/app/redux/reducers.ts
+++ src/app/redux/reducers.ts
@@ -1,8 +1,10 @@
 import {routerReducer as routing} from 'react-router-redux';
 import {combineReducers} from 'redux';
 import {CatalogReducer} from 'redux/modules/catalog';
+const reduxAsyncConnect = require('redux-connect').reducer;
 
 export default combineReducers({
+  reduxAsyncConnect,
   routing,
   catalog: CatalogReducer,
 });
--- src/client.tsx
+++ src/client.tsx
@@ -2,29 +2,35 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
-import {AppContainer} from 'react-hot-loader';
-import Root from './root';
+//import {AppContainer} from 'react-hot-loader';
+//import Root from './root';
 import configureRoutes from './app/routes';
 import configureStore from './app/redux/store';
 import './app/styles/main.less';
 
 
+const { ReduxAsyncConnect } = require('redux-connect');
+import {Provider} from 'react-redux';
+import {Router} from 'react-router';
+
+
 const store = configureStore(browserHistory, window.__INITIAL_STATE__);
 const history = syncHistoryWithStore(browserHistory, store);
 const routes = configureRoutes(store);
+const connectedCmp = (props: any) => <ReduxAsyncConnect {...props} />;
 
-const render = (Component: any) => {
+const render = () => {
   ReactDOM.render(
-    <AppContainer>
-      <Component history={history} routes={routes} store={store} />
-    </AppContainer>,
+    <Provider store={store} key="provider">
+      <Router history={history} render={connectedCmp} >{routes}</Router>
+    </Provider>,
     document.getElementById('app')
   );
 };
 
 
-render(Root);
-if ((module as any).hot) {
+render();
+/*if ((module as any).hot) {
     (module as any).hot.accept('./root', () => {render(Root);});
-}
+}*/
 
